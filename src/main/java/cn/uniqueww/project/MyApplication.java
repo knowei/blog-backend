67813a3ebcca8472f2e2bd546fadacb609f54fa8
uniqueww <poseidonll666@gmail.com>
2022-12-02 09:58:57 +0800

    project init

---
+++ src/main/java/cn/uniqueww/project/MyApplication.java
@@ -0,0 +1,15 @@
+package cn.uniqueww.project;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("cn.uniqueww.project.mapper")
+public class MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/cn/uniqueww/project/mapper/PostMapper.java
@@ -0,0 +1,18 @@
+package cn.uniqueww.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import cn.uniqueww.project.model.entity.Post;
+
+/**
+* @author yupili
+* @description 针对表【post(帖子)】的数据库操作Mapper
+* @createDate 2022-09-13 16:03:41
+* @Entity cn.uniqueww.project.model.entity.Post
+*/
+public interface PostMapper extends BaseMapper<Post> {
+
+}
+
+
+
+
